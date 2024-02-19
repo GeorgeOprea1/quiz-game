5d29951c842034ae05c24e924d00f4500882a92c
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-19 16:25:47 +0200

    style:made the design responsive for settings and game over component

--- millionaire-quiz-game/src/components/Settings.jsx
+++ millionaire-quiz-game/src/components/Settings.jsx
@@ -15,34 +15,39 @@ function Settings({
         <div className="logo-container">
           <img src="/millionaire.svg" alt="logo" className="logo" />
         </div>
-        <div className="category-container">
-          <h2>Select Category:</h2>
-          <select value={questionCategory} onChange={handleCategoryChange}>
-            <option>All</option>
-            {options &&
-              options.map((option) => (
-                <option value={option.id} key={option.id}>
-                  {option.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="difficulty-container">
-          <h2>Select Difficulty:</h2>
-          <select value={questionDifficulty} onChange={handleDifficultyChange}>
-            <option value="" key="difficulty-0">
-              All
-            </option>
-            <option value="easy" key="difficulty-1">
-              Easy
-            </option>
-            <option value="medium" key="difficulty-2">
-              Medium
-            </option>
-            <option value="hard" key="difficulty-3">
-              Hard
-            </option>
-          </select>
+        <div className="selections-container">
+          <div className="category-container">
+            <h2>Select Category:</h2>
+            <select value={questionCategory} onChange={handleCategoryChange}>
+              <option>All</option>
+              {options &&
+                options.map((option) => (
+                  <option value={option.id} key={option.id}>
+                    {option.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="difficulty-container">
+            <h2>Select Difficulty:</h2>
+            <select
+              value={questionDifficulty}
+              onChange={handleDifficultyChange}
+            >
+              <option value="" key="difficulty-0">
+                All
+              </option>
+              <option value="easy" key="difficulty-1">
+                Easy
+              </option>
+              <option value="medium" key="difficulty-2">
+                Medium
+              </option>
+              <option value="hard" key="difficulty-3">
+                Hard
+              </option>
+            </select>
+          </div>
         </div>
         <div className="button-container">
           <button id="getStarted-btn" onClick={handleStartClick}>
